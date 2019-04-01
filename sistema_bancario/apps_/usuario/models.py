7f36ec79612f861b9f13ebb802bfc3d839c7e091
Jeanca64091 <jeancarlo64091@gmail.com>
2019-03-31 20:47:50 -0600

    Creacion de los modelos Usuario y Rol - 201504394

--- sistema_bancario/apps_/usuario/models.py
+++ sistema_bancario/apps_/usuario/models.py
@@ -3,13 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
+    full_name = models.CharField(max_length=30, unique=True)
+    nick_name = models.CharField(max_length=12, unique=True)
+    correo = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=8, unique=True)
+    monto = models.DecimalField(max_digits=5, decimal_places=2)
     rol = models.ForeignKey(Rol, null=False, blank=False, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=10, unique=True)
-    telefono = models.CharField(max_length=15, unique=True)
-    dir = models.CharField(max_length=40)
-    correo_e = models.CharField(max_length=20, unique=True)
-    foto = models.FileField(upload_to='Images/%Y_%m_%d')
-    password = models.CharField(max_length=30)
-    fecha_reg = models.DateField(auto_now=False, auto_now_add=False)
     def __unicode__(self):
-        return '{}({})'.format(self.nombre, self.correo_e)
+        return '{}({})'.format(self.nick_name, self.correo_e)
+
+class Rol(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50)
+    def __unicode__(self):
+        return '{}'.format(self.nombre)
