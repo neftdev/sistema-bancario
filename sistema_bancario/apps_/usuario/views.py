9971fbd28129bddd0884ba73391a86644574255c
NeftXx <ronaldnef1996@gmail.com>
2019-04-02 22:28:42 -0600

    201504420

--- sistema_bancario/apps_/usuario/views.py
+++ sistema_bancario/apps_/usuario/views.py
@@ -57,8 +57,10 @@ def loginView(request):
 
             # CREACIONES DE VARIABLE DE SESION
             request.session["cod_cuenta"] = str(objects[0].pk)
-            
+
             print("Rol: #"+str(objects[0].pk)+"#")
+            print("Rol2: #"+str(rol)+"#")
+
             if rol == 1:
                 request.session["rol"] = True
                 return redirect('admin:home')
