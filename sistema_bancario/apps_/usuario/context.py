229da626298f478b382ac92c166e44d02cb154eb
NeftXx <ronaldnef1996@gmail.com>
2019-04-02 20:55:18 -0600

    Se arreglo bug de login - 201504420

--- sistema_bancario/apps_/usuario/context.py
+++ sistema_bancario/apps_/usuario/context.py
@@ -2,8 +2,10 @@ from .models import Usuario
 
 
 def usuarioLogueado(request):
-    codigo = request.session["cod_cuenta"]
-    usuario = Usuario.objects.filter(cod_usuario=codigo).first()
+    usuario = None
+    if "cod_cuenta" in request.session:
+        codigo = request.session["cod_cuenta"]
+        usuario = Usuario.objects.filter(cod_usuario=codigo).first()
     return {
         'usuario_logueado': usuario,
     }
