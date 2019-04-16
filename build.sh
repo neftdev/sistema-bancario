54c89c490ecda0145b61fdb10a0664a4510451c1
NeftXx <ronaldnef1996@gmail.com>
2019-04-15 19:59:32 -0600

    Configuracion de build

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@
 virtualenv -p python3 myenv
 source myenv/bin/activate
 pip3 install -r requirements.txt
-mkdir reports
-cd reports
-touch junit.xml
-touch cover.xml
-touch pep8.report
 cd ..
 cd sistema_bancario
 python3 manage.py jenkins — enable-coverage
--- sistema_bancario/sistema_bancario/settings.py
+++ sistema_bancario/sistema_bancario/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps_.usuario',
+    'django_jenkins',
 ]
 
 JENKINS_TASKS = (
