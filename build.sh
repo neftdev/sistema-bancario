01d55297a6ad29a569a268a2fef1b3103d57f65c
NeftXx <ronaldnef1996@gmail.com>
2019-04-15 20:07:51 -0600

    configuracion de build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ virtualenv -p python3 myenv
 source myenv/bin/activate
 pip3 install -r requirements.txt
 cd sistema_bancario
-python3 manage.py jenkins — enable-coverage
+python manage.py jenkins --enable-coverage
