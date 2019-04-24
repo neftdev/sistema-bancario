f7c8eacf5eb86a3dec1814190edf84f11bb8b65d
NeftXx <ronaldnef1996@gmail.com>
2019-04-23 22:20:39 -0600

    Se agrego impresion de reporte de estado de cuenta - 201504420

--- sistema_bancario/apps_/usuario/urls.py
+++ sistema_bancario/apps_/usuario/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import registroView, loginView, codigoView, homeView, transferenciaView, creditoView, notificacion_delete
+from .views import registroView, loginView, codigoView, homeView, transferenciaView, creditoView, notificacion_delete, crear_reporte
 
 urlpatterns = [
 	path('login', loginView, name = 'login'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('home', homeView, name='home'),
     path('transferencia', transferenciaView, name='transferencia'),
     path('credito', creditoView, name='credito'),
-    path('notificacion/delete/<int:cod_notificacion>', notificacion_delete, name="notificacion_delete")
+    path('notificacion/delete/<int:cod_notificacion>', notificacion_delete, name="notificacion_delete"),
+    path('mi_estado', crear_reporte, name="Reporte")
 ]
