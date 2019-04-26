64e0e2ea60d0701c2bc6b6069c35fb27df3452c8
Jeanca64091 <jeancarlo64091@gmail.com>
2019-04-26 11:53:15 -0600

    Error lab - 201504394

--- sistema_bancario/sistema_bancario/settings.py
+++ sistema_bancario/sistema_bancario/settings.py
@@ -137,4 +137,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'));
